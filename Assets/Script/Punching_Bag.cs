b67e7135ad17c46b4c013a6e1ec18e2d4412e794
MatthMatNoel <matthieu.noel@ecal.ch>
2026-02-11 15:19:03 +0100

    Punching bag + glove + sound punch

---
+++ Assets/Script/Punching_Bag.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Punching_Bag : MonoBehaviour
+{
+    [Header("Follower Number")]
+    [SerializeField] private int followerNumber = 50;
+
+    [Header("Punch Sound")]
+    [SerializeField] private AudioSource punchAudioSource;
+    [SerializeField] private AudioClip punchSound;
+
+    // Minimum force required to register as a punch
+    public float minPunchForce = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Detect collision with the punching bag
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Calculate the impact force
+        float impactForce = collision.relativeVelocity.magnitude;
+
+        // Check if the impact is strong enough to be considered a punch
+        if (impactForce >= minPunchForce)
+        {
+            punchAudioSource.pitch = Random.Range(0.7f, 1.3f);
+            punchAudioSource.volume = Random.Range(0.7f, 1.0f);
+            punchAudioSource.PlayOneShot(punchSound);
+
+            if (FollowersManager.Instance != null)
+            {
+                FollowersManager.Instance.AddFollowers(followerNumber);
+            }
+            else
+            {
+                Debug.LogWarning("FollowersManager.Instance est null : aucun gestionnaire de followers trouvé dans la scène.");
+            }
+        }
+    }
+}
